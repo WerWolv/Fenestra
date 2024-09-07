fa544de34406b9b6057fd2ac5d163e9a8738086b
WerWolv <werwolv98@gmail.com>
2024-09-07 15:33:46 +0200

    build: Fix linking aginst xdgpp

--- cmake/modules/FenestraLibrary.cmake
+++ cmake/modules/FenestraLibrary.cmake
@@ -1,6 +1,6 @@
 macro(add_fenestra_library)
     add_library(${ARGN})
 
-    target_link_libraries(${ARG0} libfenestra)
-    target_compile_definitions(${ARG0} PUBLIC IMWIN_PROJECT_NAME="${ARG0}")
+    target_link_libraries(${ARG0} PRIVATE libfenestra)
+    target_compile_definitions(${ARG0} PRIVATE FENESTRA_PROJECT_NAME="${ARG0}")
 endmacro()
